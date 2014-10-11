1eda7b6c0d6e94b474f8f6ef2f4f36eb873ee9bd
blacktail <mr.zhangping@gmail.com>
2014-10-12 00:06:44 +0800

    add default index page

--- main.js
+++ main.js
@@ -32,8 +32,7 @@ define([
 	var page = /\/(.*?)\.html/.exec(window.location.pathname);
 
 	if (!page || !page[1]) {
-		alert('The page you are visiting can not be accessible, please check your URL!');
-		return;
+		page = 'index';
 	}
 
 	page = page[1];
