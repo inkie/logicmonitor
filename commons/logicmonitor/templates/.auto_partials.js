03d72e00a88aa84bb9c05f077ab7e0d73f772098
blacktail <mr.zhangping@gmail.com>
2014-10-13 11:14:06 +0800

    update

---
+++ commons/logicmonitor/templates/.auto_partials.js
@@ -0,0 +1,3 @@
+define(["handlebars","text!commons/logicmonitor/templates/partials/placeholder.html"], function (Handlebars,arg0) {
+	Handlebars.registerPartial("commons/logicmonitor/placeholder", arg0);
+});
