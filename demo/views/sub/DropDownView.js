922927bf76aa6cf6b46c7900f223fff1e5c22cb6
gary.liu <gary.liu@logicmonitor.com>
2014-10-28 09:08:45 +0800

    add class page-wrapper

--- demo/views/sub/DropDownView.js
+++ demo/views/sub/DropDownView.js
@@ -4,6 +4,7 @@ define([
 	'./MyDropDown'
 ], function (LM, templates,  MyDropDown) {
 	return LM.View.extend({
+		className: 'page-wrapper',
 		template: templates['demo/DropDown'],
 
 		render: function () {
